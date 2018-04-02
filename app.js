c667ad706d0f08281d514f355b279f0f630f1650
Sayed Khan <arsalon47@gmail.com>
2018-04-02 14:56:55 -0400

    finished drills

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 'use strict';
 
-function whoAmI(name, age) {
-  let yearOfBirth = 2018 - age;
-  console.log(`Hi, my name is ${name} and I'm ${age} years old`);
-  console.log(`I was born in ${yearOfBirth}`)
+function yearOfBirth(age) {
+  if (age < 0) {
+    throw new Error('Age can not be negative');
+  }
+  return 2018 - age;
 }
 
-whoAmI('Robert', 30);
+function whoAmI(name, age) {
+  if (!name || !age || typeof(age) !== 'number' || typeof(name) !== 'string') {
+    console.error('Arguments not valid');
+  } else {
+    let yob;
+    try {
+      yob = yearOfBirth(age);
+      console.log(`Hi, my name is ${name} and I'm ${age} years old`);
+      console.log(`I was born in ${yob}`);
+    }
+    catch(error) {
+      console.log('' + error);
+    }
+  }
+}
+
+whoAmI('Robert', -21);
+
